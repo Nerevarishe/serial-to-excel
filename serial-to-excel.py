83beaba878cc8f1f7a0f6a7025eb5d6bd646bf4e
Link <Nerevarishe@users.noreply.github.com>
2018-04-01 16:41:18 +0300

    Создание экселевских файлов

--- serial-to-excel.py
+++ serial-to-excel.py
@@ -1,5 +1,8 @@
 import serial
+import datetime
 import time
+import xlsxwriter
+
 
 ser = serial.Serial(
     port='COM3',\
@@ -9,19 +12,30 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS,\
         timeout=0)
 
-print("connected to: " + ser.portstr)
+#print("connected to: " + ser.portstr)
 
-#this will store the line
+# this will store the line
 seq = []
 count = 1
 
+# Создание новой excel книги. В качестве имени книги используется дата и время создания
+wbName = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S") + '.xlsx'
+wb = xlsxwriter.Workbook(wbName)
+ws = workbook.add_worksheet()
+
+# Выставляем курсор в первую ячейку (A1)
+row = 0
+col = 0
+
+
 while True:
     for c in ser.read():
-        seq.append(chr(c)) #convert from ANSII
+        seq.append(chr(c)) #convert from ASCII
         joined_seq = ''.join(str(v) for v in seq) #Make a string from array
 
         if chr(c) == '\n':
-            print("Line " + str(count) + ': ' + joined_seq)
+            ws.write(row, col, joined_seq)
+            row += 1
             seq = []
             count += 1
             break
